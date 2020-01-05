c0169dfcdfb1f05820aaf068ccb5f57b93579a1c
Seph <paulo.seph@hotmail.com>
2020-01-05 13:31:02 -0300

    Eventos

--- interacaoComDom/desafio-01/app.js
+++ interacaoComDom/desafio-01/app.js
@@ -5,7 +5,8 @@ new Vue ({
         nome: "Joaquin",
         idade: 22,
         linkgoogle: "https://i2.wp.com/www.nerdsite.com.br/wp-content/uploads/2019/11/1_00001.jpg?resize=600%2C400&ssl=1",
-        nome2: "Joaqui alfred"
+        nome2: "Joaqui alfred",
+        nome3: ""
 
 
     },
