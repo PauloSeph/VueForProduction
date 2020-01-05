6d245ac340aa8706613bbef401ea76dbe372b5c1
Seph <paulo.seph@hotmail.com>
2020-01-05 19:58:02 -0300

    Condicionais, Diretivas, v-IF e v- For v-show

---
+++ Condicionais/desafio-05-resp/app.js
@@ -0,0 +1,17 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		expressao: true,
+		nomes: ['Alfred', 'Mustafa', 'Lucas', 'Pedro'],
+		livro: {
+			titulo: 'Harry Potter',
+			autor: 'i Dont now',
+			volume: '1'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21, ]
+		}
+	}
+});
