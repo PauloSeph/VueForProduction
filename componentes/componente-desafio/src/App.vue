2d4af3bd6d58bf8f3ad9fd82886916467e0b4953
Seph <paulo.seph@hotmail.com>
2020-01-07 10:01:17 -0300

    Componente Desafio

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <v-app>
+    <menu-comp></menu-comp>
+
+    <toolbar-comp></toolbar-comp>
+
+    <carrocel-comp></carrocel-comp>
+
+    <footer-comp />
+  </v-app>
+</template>
+
+<script>
+import Footer from "./Footer.vue";
+import Carrocel from "./Carrocel.vue";
+import Toolbar from "./Toolbar.vue";
+import Menu from "./Menu.vue";
+
+export default {
+  components: {
+    FooterComp: Footer,
+    CarrocelComp: Carrocel,
+    ToolbarComp: Toolbar,
+    MenuComp: Menu
+  }
+};
+</script>
