cae7919ed9855ee37ebfc0051e0fcdc6d520872a
Seph <paulo.seph@hotmail.com>
2020-01-08 18:20:05 -0300

    Comp Avancados parte 1 Configurando

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="citacao">
+        {{citacao}}
+        <slot></slot>
+    </div>
+</template>
+
+
+<script>
+export default {
+props: {
+    citacao: Array,
+}
+
+
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
