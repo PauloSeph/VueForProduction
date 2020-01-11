828834f8e46cd87c158f5d66c4c52ea7e180dd5c
Seph <paulo.seph@hotmail.com>
2020-01-11 13:03:19 -0300

    End filtrosMixins

---
+++ filtrosEmixins/filtros-mixins-desafio/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins (Desafio)</h1>
+		<hr>
+		<h1>Exercicio 1</h1>
+		<!-- Exercício 1 -->
+		<!-- Construir um filtro local que troca espaços por vírgula -->
+		<p>{{frase | espaco-por-virgula}}</p>
+		<p>{{frase | espacoPorVirgula}}</p>
+
+		<hr>
+
+
+		<!-- Exercício 2 -->
+		<!-- Filtro global que conta o tamanho de cada palavra e adiciona o 
+			valor na string final -->
+		<!-- "Pedro é legal" => "Pedro (5) é (1) legal (5)" -->
+
+			<h1>Exercicio 2</h1>
+		<p>{{frase | contarPalavras}}</p>
+
+		
+		<hr>
+
+
+		<!-- Exercício 3 -->
+		<!-- Implementar os exercicios 1 e 2 com propriedade computada -->
+			<h1>Exercicio 3</h1>
+		<p>{{fraseComVirgulas}}</p>
+		<p>{{fraseComTamanhos}}</p>
+	
+
+
+
+		<!-- Exercício 4 -->
+		<!-- Compartilhe a propriedade computada via mixin -->
+
+	</div>
+</template>
+
+<script>
+import fraseMixin from './fraseMixin'
+
+export default {
+	mixins: [fraseMixin],
+	data() {
+		return {
+			frase: 'Essa é a frase usada',
+		}
+	},
+	// Usando regExp
+	filters: {
+		espacoPorVirgula(valor) {
+			return valor.replace(/\s/g, ',')
+
+		}
+	},
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
